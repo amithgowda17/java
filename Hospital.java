99ff9a63f7fd9d4a9cebcf31e70ecb26d0d07348
xworkzdev3@gmail.com <xworkzdev3@gmail.com>
2025-08-11 15:02:35 +0530

    java 2 program

---
+++ Hospital.java
@@ -0,0 +1,18 @@
+class ApolloHospital{
+	 public static void main(String[] args){
+		 
+		String locationName="Hello world!!!";
+		
+		String addressOfHospital="Vijaynagar";
+		 int vale=8765;
+		 
+		 System.out.println("location");
+		 System.out.println("Namasthe java!!!!!!");
+		 System.out.println("Namasthe java!!!!!!");
+		 System.out.println("price");
+		
+	 }	
+}
+
+
+
